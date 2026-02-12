0edfa6e4a6daeaf16633b25aea4ccf926d822b7c
Victor <bissolivictor80@gmail.com>
2026-02-12 11:07:55 -0300

    ex da aula 1 .txt

---
+++ teste.py
@@ -0,0 +1,10 @@
+usuario = input('qual o seu nome? ')
+print('ola ',usuario,' seja bem vindo')
+senha = input('coloque sua senha' )
+print(senha)
+dominio = input('digite seu email ')
+print('seu email é:',dominio,)
+
+print('bem vindo' ,usuario, 'seu email é', usuario + "@" + dominio)
+
+
